2556c8e6dd7c88e3a5e627c869628e1f0e5019e4
HuuKhanh2003 <huynhvuonghuukhanhb6@gmail.com>
2024-11-16 23:49:34 +0700

    commit

---
+++ API_KeoDua/Reponsitory/Interface/IHoaDonBanHangReponsitory.cs
@@ -0,0 +1,11 @@
+﻿using API_KeoDua.Data;
+using API_KeoDua.DataView;
+
+namespace API_KeoDua.Reponsitory.Interface
+{
+    public interface IHoaDonBanHangReponsitory
+    {
+        public int TotalRows { get; set; }
+        public Task<List<HoaDonBanHang>> GetAllSaleInVoiceWithWait(string searchString, int startRow, int maxRows);
+    }
+}
