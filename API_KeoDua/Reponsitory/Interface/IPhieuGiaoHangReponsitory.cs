d40638bd7eda2c379bb8538ce356653cb2cd9563
GiaHoang01 <gianh1906@gmail.com>
2024-11-28 12:02:10 +0700

    commit

--- API_KeoDua/Reponsitory/Interface/IPhieuGiaoHangReponsitory.cs
+++ API_KeoDua/Reponsitory/Interface/IPhieuGiaoHangReponsitory.cs
@@ -6,6 +6,11 @@ namespace API_KeoDua.Reponsitory.Interface
     public interface IPhieuGiaoHangReponsitory
     {
         public int TotalRows { get; set; }
-        public Task<List<PhieuGiaoHang>> GetAllShippingNote(DateTime fromDate, DateTime toDate, string searchString, int startRow, int maxRow);
+        public Task<List<dynamic>> GetAllShippingNote(DateTime fromDate, DateTime toDate, string searchString, int startRow, int maxRow);
+        public Task<dynamic> GetAllShippingNoteByID(Guid? maPhieuGiao);
+        public Task<List<dynamic>> GetAllShippingNoteNewCreated(DateTime fromDate, DateTime toDate, string searchString, int startRow, int maxRow);
+        public Task<bool> AddShippingNote(PhieuGiaoHang phieuGiaoHang);
+        public Task<bool> UpdateShippingNote(PhieuGiaoHang phieuGiaoHang);
+        public Task<bool> DeleteShippingNote(Guid maPhieuGiao);
     }
 }
